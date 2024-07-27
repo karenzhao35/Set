b8c929803605e4cacb58964a8c8216d373d77b38
karenzhao35 <karenzhao35@gmail.com>
2024-07-27 16:31:18 -0700

    Initial Commit

---
+++ Set/SetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetApp.swift
+//  Set
+//
+//  Created by Karen Zhao on 2024-07-27.
+//
+
+import SwiftUI
+
+@main
+struct SetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
